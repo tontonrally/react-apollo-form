6c75c3d752a3a301a3e631e97a0d3cfcf77dd649
Christophe Lemonnier <clemonnier@sync.tv>
2018-10-30 15:51:17 +0000

    workaround due to lacks in react-jsonschema-form definition

--- dist/lib/declarations.d.ts
+++ dist/lib/declarations.d.ts
@@ -1,5 +1,4 @@
-declare module '*.json' {
-    const value: {};
-    export = value;
+import { JSONSchema6 } from "json-schema";
+declare module "react-jsonschema-form/lib/utils" {
+    function retrieveSchema(schema: JSONSchema6, declarations?: any, formData?: any): any;
 }
-declare module 'react-jsonschema-form/lib/utils';
